8ac9cf4a8367bc612bea01c8a5259cf35ce396ce
mohammadmunna13 <mohammadmuna4141@gmail.com>
2023-02-18 21:52:08 +0600

    Using Function To Calculate the values from Js

--- assigenment-5.js
+++ assigenment-5.js
@@ -3,3 +3,30 @@ document.getElementById("blogButton").onclick = function () {
     location.href = "4QuestionAnswer.html";
 };
 
+// get the value and parsefloat
+function getInputValueByID(inputId)
+{
+    const inputField = document.getElementById(inputId);
+    const inputField_string = inputField.value;
+    const inputFieldValue = parseFloat(inputField_string);
+    inputField.value='';
+    return inputFieldValue;
+}
+// set the value and show 
+function SetTextElementValueById(elementId, newValue){
+    const TextElement = document.getElementById(elementId);
+    TextElement.innerText = newValue;
+}
+
+// getting values using the function
+
+document.getElementById('Triangle_Calculate_Button').addEventListener
+('click',function()
+{
+    const baseValue = getInputValueByID('bValue');
+    const hightValue = getInputValueByID('hValue');
+    const TriangleArea= hightValue * baseValue;
+    const TriangleAreaValue = TriangleArea.value;
+
+
+})
