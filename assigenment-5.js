f45690f6d1f4704fff774803c8ec575504a2fd3d
mohammadmunna13 <mohammadmuna4141@gmail.com>
2023-02-18 23:33:31 +0600

    calculation part2

--- assigenment-5.js
+++ assigenment-5.js
@@ -12,10 +12,13 @@ function getInputValueByID(inputId)
     inputField.value='';
     return inputFieldValue;
 }
+
 // set the value and show 
 function SetTextElementValueById(elementId, newValue){
     const TextElement = document.getElementById(elementId);
     TextElement.innerText = newValue;
+    // console.log(TextElement,newValue);
+
 }
 
 // getting values using the function
@@ -27,11 +30,21 @@ document.getElementById('Triangle_Calculate_Button').addEventListener
     const baseValue = getInputValueByID('bValue');
     const hightValue = getInputValueByID('hValue');
     const TriangleArea= hightValue*baseValue;
-    const TriangleAreaValue = TriangleArea.value;
+    SetTextElementValueById('Calculate_Values1',TriangleArea);
 
+}
+)
 
-    SetTextElementValueById('Calculate_Values1',TriangleAreaValue);
+//Rectangle_Calculate_Button
 
+document.getElementById('Rectangle_Calculate_Button').addEventListener
+('click',function()
+
+{
+    const WValue = getInputValueByID('wvalue');
+    const LValue = getInputValueByID('lvalue');
+    const Rectangle= WValue*LValue;
+    SetTextElementValueById('Calculate_Values4',Rectangle);
 
 }
 )
@@ -44,10 +57,56 @@ document.getElementById('Parallelogram_Calculate_Button').addEventListener
 {
 
 const ParallelogramCalculation = 10*12;
-const ParallelogramCalculateValue = ParallelogramCalculate;
+const ParallelogramCalculateValue = ParallelogramCalculation;
 
 SetTextElementValueById('Calculate_Values3',ParallelogramCalculateValue);
 
+}
+)
+
+// Rhombus_Calculate_Button
+
+document.getElementById('Rhombus_Calculate_Button').addEventListener
+('click',function()
+
+{
+
+const RhombusCalculation = 16*8*0.5;
+const RhombusCalculateValue = RhombusCalculation;
+
+SetTextElementValueById('Calculate_Values5',RhombusCalculateValue);
+
+}
+
+)
+
+//Pentagon
+document.getElementById('Pentagon_Calculate_Button').addEventListener
+('click',function()
+
+{
+
+const PentagonCalculation = 0.5*6*10;
+const PentagonCalculateValue = PentagonCalculation;
+
+SetTextElementValueById('Calculate_Values6',PentagonCalculateValue);
+
+}
+
+)
+
+
+//Ellipse
+
+document.getElementById('Ellipse_Calculate_Button').addEventListener
+('click',function()
+
+{
+
+const EllipseCalculation = 3.1416*10*4;
+const EllipseCalculateValue = EllipseCalculation;
+
+SetTextElementValueById('Calculate_Values2',EllipseCalculateValue);
 
 }
 
